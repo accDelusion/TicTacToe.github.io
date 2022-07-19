ef86ffe6f633d7dd881e7fc0d1146bb7b650c4c3
accDelusion <89416349+accDelusion@users.noreply.github.com>
2022-07-19 16:09:21 -0400

    Add files via upload

---
+++ _nuxt/static/1658261312/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FTicTacToe.github.io\u002F_nuxt\u002Fstatic\u002F1658261312",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002FTicTacToe.github.io\u002F",assetsPath:"\u002FTicTacToe.github.io\u002F_nuxt\u002F",cdnURL:a}}}}(null));
